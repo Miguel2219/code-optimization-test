601a019762581995a6ff250d5571afc4a2d0353d
Miguel Angel <lmangelryl1512@gmail.com>
2024-09-17 07:53:54 -0500

    termine una cosa

--- src/Store.java
+++ src/Store.java
@@ -4,26 +4,29 @@ public class Store {
         Scanner escaner = new Scanner(System.in);
         float [] preciosProductos= new float [3];
         int [] cantidades= new int [3];
+        float totalSales = total1 + total2 + total3;
         final int numeroProductos=3;
 
-        for (int j = 1; j <= numeroProductos; j++) {
+        for (int j = 0; j < numeroProductos; j++) {
             System.out.println("Ingrese el precio del producto "+j);
             float producto=escaner.nextInt();
             preciosProductos[j-1]=producto;
 
             
         }
-        for (int i = 1; i<=numeroProductos; i++) {
+        for (int i = 0; i<numeroProductos; i++) {
             System.out.println("Ingrese la cantidad el producto "+i);
             int cantidad=escaner.nextInt();
             cantidades[i-1]=cantidad;
 
         }
-    
-        float total1 = (preciosProductos[0])*cantidades[0];
-        float total2 = (preciosProductos[1])*cantidades[1];
-        float total3 = (preciosProductos[2]*cantidades[2]);
-        float totalSales = total1 + total2 + total3;
+        for (int h = 0; h < cantidades.length; h++) {
+            
+
+
+
+        }
+        
 
         if (totalSales > 50) {
             System.out.println("Good sales performance");
--- src/TaxCalculation.java
+++ src/TaxCalculation.java
@@ -3,29 +3,33 @@ public class TaxCalculation {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Ingrese la cantidad de productos");
-        final int cantidadProductos=3;
+        int cantidadProductos=sc.nextInt();
         float [] preciosProducts= new float[cantidadProductos];
         float [] tasaProductos =new float[cantidadProductos];
+        float tasaTotal=0;
         for (int i = 0; i < cantidadProductos; i++) {
-            System.out.println("Ingrese el precio del producto"+(i+1));
+            System.out.println("Ingrese el precio del producto "+(i+1));
             float preciosProducto=sc.nextFloat();
             preciosProducts[i]=preciosProducto;
 
         }
         for (int j= 0; j < cantidadProductos; j++) {
-            System.out.println("Ingrese la tasa del producto 1"+(j+1));
+            System.out.println("Ingrese la tasa del producto "+(j+1));
             float tasaProducto=sc.nextFloat();
             tasaProductos[j]=tasaProducto;
 
         }
 
-        float tasa1=preciosProducts[0]+tasaProductos[0];
-        float tasa2=preciosProducts[1]+tasaProductos[1];
-        float tasa3=preciosProducts[2]+tasaProductos[2];
-        float totalTax=tasa1+tasa2+tasa3;
+        for (int h = 0; h < cantidadProductos; h++) {
+            float tasa;
+            tasa=preciosProducts[h]*tasaProductos[h];
+            tasaTotal+=tasa;
+
+        }
+        System.out.println("El total de la tasa es: "+tasaTotal);
         
-        if (totalTax > 50) {
-            System.out.println("High total tax: " + totalTax);
+        if (tasaTotal > 50) {
+            System.out.println("High total tax: " + tasaTotal);
         } else {
             System.out.println("Low tax");
         }
